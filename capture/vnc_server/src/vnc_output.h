ef9a5d7c931c166d62e3fb55c4b3ee0f076e2cf4
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2026-04-01 11:35:07 +0200

    add VNC server
    genodelabs/genode-world#432

---
+++ capture/vnc_server/src/vnc_output.h
@@ -0,0 +1,369 @@
+/*
+ * \brief  Vnc framebuffer output
+ * \author Johannes Schlatow
+ * \date   2021-05-28
+ */
+
+/*
+ * Copyright (C) 2021 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#ifndef _VNC_OUTPUT_H_
+#define _VNC_OUTPUT_H_
+
+/* Genode includes */
+#include <libc/component.h>
+#include <capture_session/connection.h>
+#include <event_session/connection.h>
+#include <util/reconstructible.h>
+#include <util/interface.h>
+
+/* 3rd-party includes */
+#include <libc/args.h>
+#include <rfb/rfb.h>
+
+/* local include */
+#include "keycode_from_keysym.h"
+
+namespace Vncserver {
+	using namespace Genode;
+
+	struct Output_control;
+	class  Output;
+}
+
+
+struct Vncserver::Output_control : Interface
+{
+	virtual void enable()  = 0;
+	virtual void disable() = 0;
+};
+
+
+class Vncserver::Output
+{
+	public:
+		struct Libvnc_error   : Exception { };
+
+		using Pixel          = Capture::Pixel;
+		using Area           = Capture::Area;
+
+	private:
+		/*
+		 * Noncopyable
+		 */
+		Output(Output const &);
+		Output &operator= (Output const &);
+
+		struct Button_state
+		{
+			enum {
+				BUTTON_1 = 0x01,
+				BUTTON_2 = 0x02,
+				BUTTON_3 = 0x04,
+				WHEEL_UP = 0x08,
+				WHEEL_DN = 0x10,
+				WHEEL_LEFT  = 0x20,
+				WHEEL_RIGHT = 0x40,
+				MASK = 0x7F
+			};
+
+			int _value;
+
+			Button_state(int value)
+			: _value(value & MASK)
+			{ }
+
+			Button_state &operator = (int value)
+			{
+				_value = value & MASK;
+				return *this;
+			}
+
+			bool operator != (Button_state const &rhs)
+			{ return _value != rhs._value; }
+
+			bool changed(Button_state const &state, int button) const
+			{ return (state._value ^ _value) & button & MASK; }
+
+			bool pressed(int button) const
+			{ return _value & button & MASK; }
+		};
+
+		Libc::Env       &_env;
+		Allocator       &_alloc;
+		Area             _area;
+		Output_control & _output_control;
+
+		Event::Connection _event_connection { _env };
+
+		Constructible<Attached_rom_dataspace> _passwd_rom { };
+
+		/* libvnc state */
+		rfbScreenInfoPtr _screen = nullptr;
+		Button_state _old_buttons { 0 };
+
+		void _realloc_buffer()
+		{
+			const size_t size = _area.w * _area.h * sizeof(Pixel);
+
+			char* oldfb = _screen->frameBuffer;
+			char* newfb = new (_alloc) char[size];
+
+			if (oldfb) {
+				rfbNewFramebuffer(_screen, newfb,
+				                  _area.w, _area.h,
+				                  8, 3, sizeof(Pixel));
+				destroy(_alloc, oldfb);
+			}
+			else
+				_screen->frameBuffer = newfb;
+
+			/* overwrite server pixel format */
+			_screen->serverFormat.redShift   = Pixel::r_shift;
+			_screen->serverFormat.greenShift = Pixel::g_shift;
+			_screen->serverFormat.blueShift  = Pixel::b_shift;
+		}
+
+		template <typename T>
+		T *_addr()
+		{
+			return static_cast<T *>((void*)_screen->frameBuffer);
+		}
+
+	public:
+
+		Output(Libc::Env &env, Allocator &alloc, Area area, Output_control & ctrl)
+		: _env(env), _alloc(alloc), _area(area), _output_control(ctrl)
+		{
+			/* rfbGetScreen takes options from argc/argv */
+			int argc    = 0;
+			char **argv = nullptr;
+			char **envp = nullptr;
+			populate_args_and_env(_env, argc, argv, envp);
+
+			_screen = rfbGetScreen(&argc, argv,
+			                       _area.w, _area.h,
+			                       8, 3, sizeof(Pixel));
+			if (!_screen)
+				throw Libvnc_error();
+
+			_screen->frameBuffer    = nullptr;
+			_screen->alwaysShared   = FALSE;
+			_screen->neverShared    = TRUE;
+
+			/**
+			 * disconnect current client if new client connects so that we
+			 * always accept a connection even if the current client has not
+			 * disconnected properly
+			 */
+			_screen->dontDisconnect = FALSE;
+
+			/* set password callback */
+			_screen->passwordCheck = [] (rfbClientPtr cl, const char* response, int len) {
+				return (static_cast<Output*>(cl->screen->screenData))
+					->check_password(cl, response, len);
+			};
+
+			/* store context for event callbacks in screenData member */
+			_screen->screenData = static_cast<void*>(this);
+
+			/* set ptr event callback */
+			_screen->ptrAddEvent = [] (int buttons, int x, int y, rfbClientPtr cl) {
+				(static_cast<Output*>(cl->screen->screenData))
+					->handle_ptr(buttons, x, y);
+				rfbDefaultPtrAddEvent(buttons, x, y, cl);
+			};
+
+			/* set keyboard event callback */
+			_screen->kbdAddEvent = [] (rfbBool down, rfbKeySym key, rfbClientPtr cl) {
+				(static_cast<Output*>(cl->screen->screenData))
+					->handle_keysym(down, key);
+			};
+
+			/* set client hook */
+			_screen->newClientHook = [] (rfbClientPtr cl) {
+				(static_cast<Output*>(cl->screen->screenData))
+					->handle_connect();
+
+				cl->clientGoneHook = [] (rfbClientPtr cl) {
+					(static_cast<Output*>(cl->screen->screenData))
+						->handle_disconnect();
+				};
+				return RFB_CLIENT_ACCEPT;
+			};
+
+			/* allocate buffer */
+			_realloc_buffer();
+
+			/* initialize the server */
+			rfbInitServer(_screen);
+
+			/* start background loop (using pthread) */
+			rfbRunEventLoop(_screen, -1, TRUE);
+		}
+
+		~Output()
+		{
+			if (_screen->frameBuffer)
+				destroy(_alloc, _screen->frameBuffer);
+
+			rfbScreenCleanup(_screen);
+		}
+
+		void resize(Area area)
+		{
+			_area = area;
+			Libc::with_libc([&] () {
+				_realloc_buffer();
+			});
+		}
+
+		void handle_config(Node const &node)
+		{
+			if (node.attribute_value("requires_password", false))
+				_screen->authPasswdData = (void *)"passwd";
+			else
+				_screen->authPasswdData = NULL;
+		}
+
+		rfbBool check_password(rfbClientPtr cl, const char* response, int len)
+		{
+			using Password = Genode::String<64>;
+
+			/* create a new ROM connection with label specified by authPasswdData */
+			try {
+				_passwd_rom.construct(_env, (char *)cl->screen->authPasswdData);
+			} catch (...) {
+				return FALSE;
+			}
+
+			/* invalid or empty dataspace -> no password */
+			if (!_passwd_rom->valid() || _passwd_rom->size() == 0)
+				return FALSE;
+
+			Password passwd(_passwd_rom->local_addr<const char>());
+
+			if (!passwd.valid())
+				return FALSE;
+
+			/* remove newline from tail */
+			char * raw_passwd = (char *)passwd.string();
+			if (raw_passwd[passwd.length()-2] == '\n')
+				raw_passwd[passwd.length()-2] = '\0';
+
+			/* encrypt passwd and store in authChallenge */
+			rfbEncryptBytes(cl->authChallenge, raw_passwd);
+
+			/* destruct ROM connection since we do not want to store password in memory */
+			_passwd_rom.destruct();
+
+			/* compare result with response */
+			if (Genode::memcmp(cl->authChallenge, response, len) != 0) {
+				rfbErr("Password authentication failed\n");
+				return FALSE;
+			}
+
+			return TRUE;
+		}
+
+		void handle_connect()    { _output_control.enable();  }
+		void handle_disconnect() { _output_control.disable(); }
+
+		void handle_ptr(int buttons, int x, int y)
+		{
+			if (x < 0 || y < 0 || (unsigned)x >= _area.w || (unsigned)y >= _area.h)
+				return;
+
+			Button_state cur_buttons(buttons);
+
+			_event_connection.with_batch([&] (Event::Session_client::Batch &batch) {
+				batch.submit(Input::Absolute_motion{x, y});
+
+				/* determine changed button states */
+				if (_old_buttons != cur_buttons) {
+					if (_old_buttons.changed(cur_buttons, Button_state::BUTTON_1)) {
+						if (cur_buttons.pressed(Button_state::BUTTON_1))
+							batch.submit(Input::Press  {Input::BTN_LEFT});
+						else
+							batch.submit(Input::Release{Input::BTN_LEFT});
+					}
+					if (_old_buttons.changed(cur_buttons, Button_state::BUTTON_2)) {
+						if (cur_buttons.pressed(Button_state::BUTTON_2))
+							batch.submit(Input::Press  {Input::BTN_MIDDLE});
+						else
+							batch.submit(Input::Release{Input::BTN_MIDDLE});
+					}
+					if (_old_buttons.changed(cur_buttons, Button_state::BUTTON_3)) {
+						if (cur_buttons.pressed(Button_state::BUTTON_3))
+							batch.submit(Input::Press  {Input::BTN_RIGHT});
+						else
+							batch.submit(Input::Release{Input::BTN_RIGHT});
+					}
+					if (_old_buttons.changed(cur_buttons, Button_state::WHEEL_UP)) {
+						if (cur_buttons.pressed(Button_state::WHEEL_UP))
+							batch.submit(Input::Wheel{0, 1});
+					}
+					if (_old_buttons.changed(cur_buttons, Button_state::WHEEL_DN)) {
+						if (cur_buttons.pressed(Button_state::WHEEL_DN))
+							batch.submit(Input::Wheel{0,-1});
+					}
+					if (_old_buttons.changed(cur_buttons, Button_state::WHEEL_LEFT)) {
+						if (cur_buttons.pressed(Button_state::WHEEL_LEFT))
+							batch.submit(Input::Wheel{-1,0});
+					}
+					if (_old_buttons.changed(cur_buttons, Button_state::WHEEL_RIGHT)) {
+						if (cur_buttons.pressed(Button_state::WHEEL_RIGHT))
+							batch.submit(Input::Wheel{1,0});
+					}
+				}
+			});
+
+			_old_buttons = buttons;
+		}
+
+		void handle_keysym(bool down, rfbKeySym keysym)
+		{
+			_event_connection.with_batch([&] (Event::Session_client::Batch &batch) {
+				bool           pass_char = character_keysym(keysym);
+				Input::Keycode keycode   = Input::KEY_UNKNOWN;
+
+				if (!pass_char) {
+					keycode = keycode_from_keysym(keysym);
+					if (keycode == Input::KEY_UNKNOWN) return;
+				}
+
+				if (down) {
+					if (pass_char)
+						batch.submit(Input::Press_char{keycode, Codepoint{keysym}});
+					else
+						batch.submit(Input::Press     {keycode});
+				}
+				else
+					batch.submit(Input::Release{keycode});
+			});
+		}
+
+		void refresh(Capture::Rect const rect)
+		{
+			Libc::with_libc([&] () {
+				rfbMarkRectAsModified(_screen, rect.x1(), rect.y1(),
+				                               rect.x2(), rect.y2());
+			});
+		}
+
+		Area area() const { return _area; }
+
+		template <typename FN>
+		void with_surface(FN const &fn)
+		{
+			Surface<Pixel> surface(_addr<Pixel>(), _area);
+
+			fn(surface);
+		}
+};
+
+#endif /* _VNC_OUTPUT_H_ */
---
+++ capture/vnc_server/src/vnc_server.cc
@@ -0,0 +1,266 @@
+/*
+ * \brief  Vnc server using libvnc
+ * \author Johannes Schlatow
+ * \date   2021-05-26
+ */
+
+/*
+ * Copyright (C) 2021 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+
+#include <base/env.h>
+#include <libc/component.h>
+#include <base/log.h>
+#include <base/attached_rom_dataspace.h>
+#include <base/heap.h>
+#include <capture_session/connection.h>
+#include <timer_session/connection.h>
+
+#include "vnc_output.h"
+
+namespace Vncserver {
+
+	using namespace Genode;
+
+	template <typename>
+	class  Rate_limiter;
+	struct Main;
+}
+
+
+template <typename HANDLER>
+class Vncserver::Rate_limiter : public Output_control
+{
+	private:
+
+		using Handler_method   = void (HANDLER::*)();
+		using One_shot_timeout = Timer::One_shot_timeout<Rate_limiter>;
+
+		Env                        & _env;
+		Capture::Connection        & _capture;
+		HANDLER                    & _handler;
+		Handler_method const         _handler_method;
+		Timer::Connection            _timer   { _env };
+		One_shot_timeout             _timeout { _timer, *this, &Rate_limiter::_handle_timeout };
+		Signal_handler<Rate_limiter> _wakeup_handler {
+			_env.ep(), *this, &Rate_limiter::_handle_wakeup };
+
+		Duration _period     { Milliseconds { 40 } };  /* max. 25 fps */
+		Duration _last_frame { Microseconds { 0 } };
+
+		bool     _idle       { true };
+
+		void _handle_timeout(Duration)
+		{
+			_last_frame = _timer.curr_time();
+			(_handler.*_handler_method)();
+			_capture.capture_stopped();
+		}
+
+		void _handle_wakeup()
+		{
+			if (_idle) return;
+
+			Duration now      = _timer.curr_time();
+			Duration earliest = _last_frame;
+			earliest.add(_period.trunc_to_plain_ms());
+			earliest.add(Milliseconds { 1 }); /* tolerance */
+			if (earliest.less_than(now)) {
+				/* call handler_method only if last call was > _period in the past */
+				_last_frame = now;
+				(_handler.*_handler_method)();
+				_capture.capture_stopped();
+			} else if (!_timeout.scheduled()) {
+				/* postpone the call of handler_method */
+				auto diff = earliest.trunc_to_plain_us().value - now.trunc_to_plain_us().value;
+				_timeout.schedule({ Microseconds { diff } });
+			}
+		}
+
+	public:
+
+		Rate_limiter(Env & env, Capture::Connection & capture,
+		             HANDLER & handler, Handler_method method)
+		: _env(env), _capture(capture),
+		  _handler(handler),
+		  _handler_method(method)
+		{
+			_capture.wakeup_sigh(_wakeup_handler);
+			_capture.capture_stopped();
+		}
+
+		void update_period(Milliseconds period) { _period = Duration({period}); }
+
+		/**
+		* Output_control interface
+		*/
+
+		void enable() override
+		{
+			_idle = false;
+			_last_frame = _timer.curr_time();
+			(_handler.*_handler_method)();
+			_capture.capture_stopped();
+		}
+
+		void disable() override
+		{
+			_timeout.discard();
+			_idle = true;
+		}
+};
+
+
+struct Vncserver::Main
+{
+	Libc::Env &_env;
+
+	using Pixel          = Capture::Pixel;
+	using Area           = Capture::Area;
+	using Affected_rects = Capture::Session::Affected_rects;
+
+	Attached_rom_dataspace    _config { _env, "config" };
+
+	Heap                      _heap   { _env.ram(), _env.rm() };
+
+	static Capture::Point _point_from_node(Node const &node)
+	{
+		return Capture::Point(node.attribute_value("xpos", 0L),
+		                      node.attribute_value("ypos", 0L));
+	}
+
+	static Area _area_from_node(Node const &node, Area default_area)
+	{
+		return Area(node.attribute_value("width",  default_area.w),
+		            node.attribute_value("height", default_area.h));
+	}
+
+	static Area _safe_screen_size(Area default_area)
+	{
+		if (default_area.w > 0 && default_area.h > 0)
+			return default_area;
+		else
+			return Area(320, 240);
+	}
+
+	Capture::Connection  _capture      { _env };
+	Rate_limiter<Main>   _rate_limiter { _env, _capture, *this, &Main::_handle_capture };
+	Output               _output       { _env,
+	                                     _heap,
+	                                     _safe_screen_size(_capture.screen_size()),
+	                                     _rate_limiter };
+
+	struct Capture_input
+	{
+		Env &_env;
+
+		Capture::Connection &_capture;
+
+		Area const _area;
+
+		bool _capture_buffer_init = ( _capture.buffer({ .px = _area,
+		                                                .mm = { },
+		                                                .viewport = { { }, _area }}), true );
+
+		Attached_dataspace _capture_ds { _env.rm(), _capture.dataspace() };
+
+		Texture<Pixel> const _texture { _capture_ds.local_addr<Pixel>(), nullptr, _area };
+
+		Capture::Point _at { };
+
+		Capture_input(Env &env,
+		              Capture::Connection &capture,
+		              Area area,
+		              Node const &config)
+		:
+			_env(env),
+			_capture(capture),
+			_area(area),
+			_at(_point_from_node(config))
+		{ }
+
+		Affected_rects capture() { return _capture.capture_at(_at); }
+
+		template <typename FN>
+		void with_texture(FN const &fn) const
+		{
+			fn(_texture);
+		}
+	};
+
+	Constructible<Capture_input> _capture_input { };
+
+	void _handle_capture()
+	{
+		if (!_capture_input.constructed())
+			return;
+
+		_capture_input->with_texture([&] (Texture<Pixel> const &texture) {
+
+			_output.with_surface([&] (Surface<Pixel> &surface) {
+				Affected_rects const affected = _capture_input->capture();
+
+				affected.for_each_rect([&] (Capture::Rect const rect) {
+
+					surface.clip(rect);
+
+					Blit_painter::paint(surface, texture, Capture::Point(0, 0));
+				});
+
+				affected.for_each_rect([&] (Capture::Rect const rect) {
+					_output.refresh(rect);
+				});
+			});
+		});
+
+	}
+
+	void _handle_resize()
+	{
+		Node const &config = _config.node();
+
+		Area area = _area_from_node(config, _safe_screen_size(_capture.screen_size()));
+
+		_output.resize(area);
+
+		_capture_input.construct(_env, _capture, area, config);
+	}
+
+	void _handle_config()
+	{
+		_config.update();
+		_output.handle_config(_config.node());
+
+		_handle_resize();
+
+		unsigned long const period_ms = _config.node().attribute_value("period_ms", 0U);
+
+		if (period_ms == 0)
+			warning("missing or invalid 'period_ms' config attribute");
+
+		_rate_limiter.update_period(Milliseconds { period_ms });
+	}
+
+	Signal_handler<Main> _resize_handler { _env.ep(), *this, &Main::_handle_resize };
+	Signal_handler<Main> _config_handler { _env.ep(), *this, &Main::_handle_config };
+
+	Main(Libc::Env &env) : _env(env)
+	{
+		_config.sigh(_config_handler);
+		_capture.screen_size_sigh(_resize_handler);
+
+		_handle_config();
+	}
+};
+
+
+void Libc::Component::construct(Libc::Env &env)
+{
+	Libc::with_libc([&] () {
+		static Vncserver::Main main(env);
+	});
+}
